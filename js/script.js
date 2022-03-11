4893126235b1e436cb28cdc173fbf0291e903efe
DeAnn Hickey <2freefly35@gmail.com>
2022-03-11 11:35:20 -0800

    Final commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,116 @@
 // STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
 // STEP 2
+// 20
+// 'this is a string literal'
+// true
+// null
+
 // STEP 3
+// var firstExpression = 20;
+// let secondExpression = firstExpression / 5;
+
 // STEP 4
+// let strFirstName, strLastName, strAddress, strCity, strState, iZipCode, iYourAge, strReferralSource, strMayWeContactYou;
+
 // STEP 5
+// var strFirstName;
+// strFirstName = 'DeAnn';
+
+// let strAddress = '1234 This Way Lane';
+
+// let iZipCode = 12345, strFirstName = 'DeAnn';
+
 // STEP 6
+// let volume = 11;
+// alert('Turn it up to ' + volume + '!');
+
 // STEP 7
+// let bTrue = true;
+// let iNum = 5;
+// alert('This is a ' + bTrue + ' statement.');
+// alert(bTrue + iNum);
+
 // STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
 // STEP 9
+// var nada = null;
+// console.log(nada);
+// let undeFined;
+// console.log(undeFined);
+
 // STEP 10
+// let strFirstName = 'DeAnn';
+// let iNum = 5;
+// let bTrue = true;
+// let undeFined;
+// let course = {typeOfCourse:'Javascript', assignment:1};
+// console.log(typeof(strFirstName));
+// console.log(typeof(iNum));
+// console.log(typeof(bTrue));
+// console.log(typeof(course));
+// console.log(typeof(undeFined));
+
 // STEP 11
+// alert('Hello ' + 'DeAnn' + ', welcome to the JavaScript class!');
+
 // STEP 12
+// let name = 'DeAnn';
+// alert(`Hello ${name}, welcome to the JavaScript class!`);
+
 // STEP 13
+// let name = 'DeAnn';
+// let course = 'JavaScript';
+// alert(`Hello ${name}, welcome to the ${course} class!`);
+
 // STEP 14
+// let name = 'DeAnn';
+// let course = 'JavaScript';
+// alert(`Hello ${name}.\nWelcome to the ${course} class!`);
+
 // STEP 15
+// let name = prompt('Enter your name');
+// let course = 'JavaScript';
+// alert(`Hello ${name}, welcome to the ${course} class!`);
+
 // STEP 16
+// let name = prompt('Enter your name');
+// let course = prompt('Enter the class');
+// alert(`Hello ${name}, welcome to the ${course} class!`);
+
 // STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
 // STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
+
 // STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
 // STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
 // STEP 21
+// let x = '100';
+// let y = 100;
+// console.log(x == y || x > y);
+
 // STEP 22
+// let x = '100';
+// let y = 100;
+// console.log(x != y && x < y);
